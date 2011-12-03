7c833f052c576a7a20e3c4e5405af75f69da360d
Carl Goodwin-Morgan <cgm@sub6.com>
2011-12-03 20:59:01 +0000

    Corrected copyright

--- htpasswd_generator.php
+++ htpasswd_generator.php
@@ -1,7 +1,18 @@
 <?php
 /*
- Written by Carl (cgm@sub6.com)
- This is used a web form to generate .htpasswd user/password lines with sha1(default),md5 and crypt options
+   Copyright 2011 Carl Goodwin-Morgan
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 */
 if (empty($_POST[username]) || empty($_POST[password])) {
 echo <<< HTML
