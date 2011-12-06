49e57c8efeb4ae4d9da8f5fc21374a085505c8d0
Carl Goodwin-Morgan <cgm@sub6.com>
2011-12-06 20:49:35 +0000

    Fixed crud.

--- htpasswd_generator.php
+++ htpasswd_generator.php
@@ -15,7 +15,7 @@
    limitations under the License.
 */
 if (empty($_POST[username]) || empty($_POST[password])) {
-echo << HTML
+echo <<< HTML
 <html>
 <head>
 <title>Generate your .htpasswd user/pass login</title>
@@ -27,14 +27,14 @@ border: 1px solid #000000; padding: 2px;
 </head>
 <body>
 <h3>Please enter your username and password below, please note we do not log either value you enter.</h3>
-<form method=post>
-<div id=form align=left>
-<span class=txtbrd> Username: <input type=text name=username size=30></span><br /><br />
-<span class=txtbrd> Password: <input type=text name=password size=30></span><br /><br />
-<span class=txtbrd>Encryption type: 
-<select name=etype>
-<option value=sha1>SHA1 (default)</option>
-<option value=crypt>Crypt</option>
+<form method='post'>
+<div id='form' align='left'>
+<span class='txtbrd'> Username: <input type='text' name='username' size='30'></span><br /><br />
+<span class='txtbrd'> Password: <input type='text' name='password' size='30'></span><br /><br />
+<span class='txtbrd'>Encryption type: 
+<select name='etype'>
+<option value='sha1'>SHA1 (default)</option>
+<option value='crypt'>Crypt</option>
 </select>
 </span>
 <br /><br />
@@ -45,8 +45,8 @@ border: 1px solid #000000; padding: 2px;
 </html>
 HTML;
 } else {
- $password = trim($_POST[password]);
-  switch ($_POST[etype]) {
+ $password = trim($_POST['password']);
+  switch ($_POST['etype']) {
    case 'crypt':
     $encrypted_password = crypt($password, substr($password,0,2));
    break;
@@ -54,25 +54,21 @@ HTML;
     $encrypted_password = '{SHA}'.base64_encode(sha1($password, TRUE));
    break;
   }
-echo << HTML
+echo <<< HTML
 <html>
 <head>
 <title>Generatated .htpasswd user/pass logins</title>
 <style type="text/css">
-.txtbrd {
-border: 1px solid #000000; padding: 2px;
-}
 </style>
 </head>
 <body>
-<span class=txtbrd>
-HTML; 
- echo "Here is the .htpasswd line <br />";
+<span class='txtbrd'>
+HTML;
+ echo "Here is the .htpasswd line:<br />";
  echo $_POST[username].":".$encrypted_password."<br />"; 
-echo << HTML
-</span>
+echo <<< HTML
 </body>
 </html>
-HTML; 
+HTML;
 }
 ?>
