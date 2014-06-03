b6c66a6234130e384b4decdb9066b1d73041e03d
Carl <cgm@sub6.com>
2014-06-03 22:22:40 +0100

    Update htpasswd_generator.php
    Updated license to MIT.
    Updated code to use html directly rather than php echo.

--- htpasswd_generator.php
+++ htpasswd_generator.php
@@ -1,21 +1,27 @@
 <?php
 /*
-   Copyright 2011 Carl Goodwin-Morgan
+Copyright (c) 2011 Carl Goodwin-Morgan
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-       http://www.apache.org/licenses/LICENSE-2.0
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 */
 if (empty($_POST['username']) || empty($_POST['password'])) {
-echo <<< HTML
+?>
 <html>
 <head>
 <title>Generate your .htpasswd user/pass login</title>
@@ -43,7 +49,7 @@ border: 1px solid #000000; padding: 2px;
 </form>
 </body>
 </html>
-HTML;
+<?php
 } else {
  $password = trim($_POST['password']);
   switch ($_POST['etype']) {
@@ -54,7 +60,7 @@ HTML;
     $encrypted_password = '{SHA}'.base64_encode(sha1($password, TRUE));
    break;
   }
-echo <<< HTML
+?>
 <html>
 <head>
 <title>Generatated .htpasswd user/pass logins</title>
@@ -62,12 +68,12 @@ echo <<< HTML
 </style>
 </head>
 <body>
-HTML;
+<?php
  echo "Here is the .htpasswd line:<br />";
  echo $_POST['username'].":".$encrypted_password."<br />"; 
-echo <<< HTML
+?>
 </body>
 </html>
-HTML;
+<?php
 }
 ?>
